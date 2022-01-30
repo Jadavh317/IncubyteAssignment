f67e76cd8ba1e7ee6c0a3d051baa1b65d437c994
jadavh317 <jadavh317@gmail.com>
2022-01-30 22:11:18 +0530

    test null string

---
+++ src/sourcecode/StringCalculator.java
@@ -0,0 +1,11 @@
+package sourcecode;
+
+public class StringCalculator {
+
+	public void addNum(String numbers) {
+		if(numbers==null) {
+			throw new IllegalArgumentException("Numbers must not be null");
+		}
+	}
+	
+}
--- src/testcase/TestJUnit.java
+++ src/testcase/TestJUnit.java
@@ -1,13 +1,23 @@
 package testcase;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 
+import sourcecode.StringCalculator;
+
 public class TestJUnit {
+	StringCalculator calculator=new StringCalculator();
 	@Test
 	public void testingJUnit() {
 		String str = "Starting Test";
 		assertEquals("Starting Test", str);
+	}
+	@Test
+	public void nullStringCheck() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			calculator.addNum(null);
+		});
 	}
 }
